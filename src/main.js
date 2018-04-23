992c8d11123a08c1d9412655005a5059b9c6310f
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-04-23 17:42:26 -0300

    add firebase to the app

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
 
+import * as firebase from 'firebase'
+
 class Init {
     constructor() {
+        this.firebase()
         const component = require('./template/index')
         let app = document.getElementById('app')
-        app.innerHTML=component.template
+        app.innerHTML = component.template
         component.action()
+    }
+
+    firebase() {
+        // Initialize Firebase
+        const config = {
+            apiKey: "AIzaSyDAFKKfNp4wc81C0zhA8Iel0pvgG4XaPKE",
+            authDomain: "xumes-hdvirtual.firebaseapp.com",
+            databaseURL: "https://xumes-hdvirtual.firebaseio.com",
+            projectId: "xumes-hdvirtual",
+            storageBucket: "xumes-hdvirtual.appspot.com",
+            messagingSenderId: "1035611836007"
+        };
+        firebase.initializeApp(config)
     }
 }
 
 
 
 
-new Init('Xumes')
+new Init()
