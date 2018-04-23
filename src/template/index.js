c6ee6869cf306313b0214de20bdb97a24f579ec4
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-04-23 17:16:40 -0300

    adjusting sidebar effect

--- src/template/index.js
+++ src/template/index.js
@@ -13,6 +13,23 @@ module.exports = {
         </div>
         ${sidebar}"`,
     action: () => {
-        console.log('done')
+        let openMenu = document.querySelector('#header .menu-icon')
+        let closeMenu = document.getElementById('wrapper')
+
+        let closeMenuEvent = (e) => {
+            let boy = document.querySelector('body')
+            body.className = body.className.replace('show-menu', '')
+        }
+
+        openMenu.addEventListener('click', (e) => {
+            e.preventDefault() //para não navegar para o link
+            let body = document.querySelector('body')
+            body.className += " show-menu"
+
+            closeMenu.addEventListener('click', (e) => {
+                let boy = document.querySelector('body')
+                body.className = body.className.replace('show-menu', '')
+            }, true)
+        })
     }
 }
